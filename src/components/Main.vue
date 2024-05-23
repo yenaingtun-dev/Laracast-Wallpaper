285a6317a2ab55c45ff87538a394ba784376e31b
Ye Naing Tun <yenaingtun425@gmail.com>
2024-05-23 21:10:07 +0630

    add search

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -14,6 +14,7 @@ export default {
             wallpapers: json.wallpapers,
             pageSize: 5,
             currentPage: 1,
+            search: '',
         }
     },
     computed: {
@@ -24,6 +25,11 @@ export default {
             const startIndex = (this.currentPage - 1) * this.pageSize;
             const endIndex = startIndex + this.pageSize;
             return this.wallpapers.slice(startIndex, endIndex);
+        },
+        filteredList() {
+            return this.wallpapers.filter(wallpaper => {
+                return wallpaper.alt.toLowerCase().includes(this.search.toLowerCase())
+            })
         }
     },
     methods: {
@@ -51,7 +57,36 @@ export default {
                             <h1 class="text-2xl font-medium">{{ title }}</h1>
                             <p class="text-grey-600 text-sm mt-2">{{ content }}</p>
                         </header>
-                        <div v-for="wallpaper in paginatedItems" :key="wallpaper.id" class="mb-12">
+                        <div class="relative text-gray-700 mb-10 w-3/4">
+                            <label for="Search" class="sr-only"> Search Laracast Course Title </label>
+
+                            <input v-model="search" type="text" id="Search" placeholder="Search Laracast Course Title"
+                                class="w-full rounded-md border-gray-200 py-2.5 pe-10 shadow-sm sm:text-sm" />
+
+                            <span class="absolute inset-y-0 end-0 grid w-10 place-content-center">
+                                <button type="button" class="text-gray-600 hover:text-gray-700">
+                                    <span class="sr-only">Search</span>
+
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="h-4 w-4">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                                    </svg>
+                                </button>
+                            </span>
+                        </div>
+                        <div v-if="search.length > 0">
+                            <div v-for="wallpaper in filteredList" :key="wallpaper.id" class="mb-12">
+                                <a :href="wallpaper.img">
+                                    <img :src="wallpaper.view" :alt="wallpaper.alt"
+                                        class="mb-4 w-full h-full rounded-md" :key="wallpaper.id" loading="lazy" />
+                                </a>
+                                <div class="flex justify-center items-center">
+                                    <a :href="wallpaper.img" class="button" download="">Download Wallpaper</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div v-else v-for="wallpaper in paginatedItems" :key="wallpaper.id" class="mb-12">
                             <a :href="wallpaper.img">
                                 <!-- <div>
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
@@ -67,7 +102,7 @@ export default {
                                 <a :href="wallpaper.img" class="button" download="">Download Wallpaper</a>
                             </div>
                         </div>
-                        <div class=" max-w-lg container flex justify-center mx-auto">
+                        <div class="max-w-lg container flex justify-center mx-auto" v-if="!search.length > 0">
                             <div class="flex flex-row mx-auto">
                                 <button type="button" @click="previousPage" :disabled="currentPage === 1"
                                     class="bg-gray-800 text-white rounded-l-md border-r border-gray-100 py-2 hover:bg-red-700 hover:text-white px-3">
